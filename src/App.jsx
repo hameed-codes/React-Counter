98a72752709c9ab5b21409c64baec8bcd73fbb45
Hameed Khan <furqanhkhan00@gmail.com>
2026-04-06 18:31:59 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Counter from './myComponents/Counter/Counter.jsx'
+
+function App() {
+
+    return (
+        <>
+            <Counter/>
+        </>
+    );  
+}
+
+export default App
---
+++ src/myComponents/Counter/Counter.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import styles from "./Counter.module.css"
+
+function Counter() {
+
+  const [count, setCount] = useState(0);
+  const [isScale, setIsScale] = useState(false);
+
+  let animate = () => {
+    setIsScale(true);
+    setTimeout(() => {
+      setIsScale(false);
+    }, 300)
+  }
+
+  let increaseCount = () => {
+    setCount(count + 1);
+  }
+
+  let decreaseCount = () => {
+    setCount(count - 1);
+  }
+
+  let resetCount = () => {
+    setCount(0);
+  }
+
+  return (
+    <>
+      <div className={`${styles.container}`}>
+        <h1 className={styles.heading} >REACT COUNTER</h1>
+        <p className={`${styles.countDisplay} ${isScale ? "scale-pop" : "" }`} >{count}</p>
+        <div className={styles.btns}>
+          <button className={styles.btn} onClick={()=> {
+            decreaseCount();
+            animate();
+          }} >-</button>
+          <button className={`${styles.resetBtn} ${styles.btn}`} onClick={()=> {
+            resetCount();
+            animate();
+          }} >RESET</button>
+          <button className={styles.btn} onClick={()=> {
+            increaseCount();
+            animate();
+          }} >+</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Counter;
